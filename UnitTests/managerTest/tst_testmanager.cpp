158dba63fdb250e967e8946366ee493c83f0574e
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2019-11-27 16:22:21 +0200

    empty template for unit tests

---
+++ UnitTests/managerTest/tst_testmanager.cpp
@@ -0,0 +1,35 @@
+#include <QtTest>
+
+// add necessary includes here
+
+class testManager : public QObject
+{
+    Q_OBJECT
+
+public:
+    testManager();
+    ~testManager();
+
+private slots:
+    void test_case1();
+
+};
+
+testManager::testManager()
+{
+
+}
+
+testManager::~testManager()
+{
+
+}
+
+void testManager::test_case1()
+{
+
+}
+
+QTEST_APPLESS_MAIN(testManager)
+
+#include "tst_testmanager.moc"
